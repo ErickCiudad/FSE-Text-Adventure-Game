9b118a29ad906206ab973796fe6b53300828b678
DESKTOP-LF2PVSQ\Erick Ciudad <ErickCiudad1@gmail.com>
2017-10-09 00:08:13 -0700

    Working on RoomGenerator, experiences stack overflow issue but working on solution (ending endless loop since room repeatedly generates since both rooms have been marked false)

--- RoomGenerator.java
+++ RoomGenerator.java
@@ -4,6 +4,10 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class RoomGenerator {
 
+	boolean forest1 = true;
+	boolean forest2 = true;
+
+
 	public void RoomGenerate () {
 		/*
 		 * Any order, but must go through most of levels first
@@ -15,6 +19,7 @@ public class RoomGenerator {
 		String setting = "";
 		
 		int level = 1;
+		int roomChoice;
 		
 		/*
 		int nbr = ThreadLocalRandom.current().nextInt(1, 3 + 1);
@@ -26,24 +31,42 @@ public class RoomGenerator {
 			//Forest
 			setting = "Forest";
 			
-			int nbr = ThreadLocalRandom.current().nextInt(1, 10 + 1);
+			int nbr = ThreadLocalRandom.current().nextInt(1, 2 + 1);
 			switch(nbr) 	{
 				case 1: {
-					System.out.println(setting + nbr);
-					System.out.println("Straight ahead there is a dirt path that leads to a clearing");
-					System.out.println("To your right, a dirt path winds down deeper into the woods");
 					
-					Main.choice = in.nextInt();
+					if(forest1 == false) {System.out.println("Already done"); RoomGenerate();}
+					forest1 = false;
 					
-					System.out.println("You are on a grassy patch of land in a circular clearing in a the forest.");
+					System.out.println("(1)Straight ahead there is a dirt path that leads to a clearing");
+					System.out.println("(2)To your right, a dirt path winds down deeper into the woods");
 					
-
-
+					//Main.choice = in.nextInt();
+					do {
+						roomChoice = in.nextInt();
+						if(roomChoice == 1) {
+							System.out.println("You are on a grassy patch of land in a circular clearing in a the forest.");
+							RoomGenerate();
+						}
+						else if(roomChoice == 2) {
+							System.out.println("You wind deeper into the forest. The light get's dimmer. The shrubbery around is dense and invading.");				
+							RoomGenerate();
+						}
+						else {
+							System.out.println("Invalid command");
+						}
+					}
+					while(roomChoice != 1 || roomChoice != 2);
 				}
 				break;
 				
 				case 2: {
+					
+					if(forest2 == false) {System.out.println("Already done"); RoomGenerate();}
+					forest2 = false;
+				
 					System.out.println(setting + nbr);
+					RoomGenerate();
 
 				}
 				break;
@@ -119,117 +142,6 @@ public class RoomGenerator {
 	
 	///Main happens here
 	
-	public static void main (String[] args) {
-		/*
-		 * Any order, but must go through most of levels first
-		 * Forest THEN Cave THEN Castle
-		 * 
-		 * */
-		
-		Scanner in = new Scanner (System.in);
-		String setting = "";
-		
-		int level = 1;
-		
-		/*
-		int nbr = ThreadLocalRandom.current().nextInt(1, 3 + 1);
-		System.out.println(nbr);
-		*/
-		
-		switch (level) {
-		case 1 : 
-			//Forest
-			setting = "Forest";
-			
-			int nbr = ThreadLocalRandom.current().nextInt(1, 10 + 1);
-			switch(nbr) 	{
-				case 1: {
-					System.out.println(setting + nbr);
-					System.out.println("Straight ahead there is a dirt path that leads to a clearing");
-					System.out.println("To your right, a dirt path winds down deeper into the woods");
-					
-					Main.choice = in.nextInt();
-					
-					System.out.println("You are on a grassy patch of land in a circular clearing in a the forest.");
-					
-
-
-				}
-				break;
-				
-				case 2: {
-					System.out.println(setting + nbr);
-
-				}
-				break;
-
-				case 3: {
-					System.out.println(setting + nbr);
-
-				}
-				break;
-
-				case 4: {
-					System.out.println(setting + nbr);
-
-				}
-				break;
-
-				case 5: {
-					System.out.println(setting + nbr);
-
-				}
-				break;
-
-				case 6: {
-					System.out.println(setting + nbr);
-
-				}
-				break;
-
-				case 7: {
-					System.out.println(setting + nbr);
-
-				}
-				break;
-
-				case 8: {
-					System.out.println(setting + nbr);
-
-				}
-				break;
-
-				case 9: {
-					System.out.println(setting + nbr);
-
-				}
-				break;
-
-				case 10: {
-					System.out.println(setting + nbr);
-
-				}
-				break;
-
-			}
-			
-			
-			break;
-			
-		case 2 : 
-			// Cave
-			System.out.println("Two");
-			break;
-			
-		case 3 : 
-			// Castle
-			System.out.println("Three");
-			break;
-			
-		
-			
-		}
-
 	}
 	
-}
+
